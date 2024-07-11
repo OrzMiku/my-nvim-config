9666908497613b7111b8e345526164c84b1e352d
OrzMiku <miku@ecy.pink>
2024-07-11 23:26:41 +0800

    New key binding Ctrl+B has been added for toggling NeoTree.

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -2,7 +2,11 @@ vim.g.mapleader = "\\"
 
 local keymap = vim.keymap
 
--- Normal Mode
+-- Normal Mode --
+-- Spilt
 keymap.set("n", "<leader>sv", "<C-w>v")
 keymap.set("n", "<leader>sh", "<C-w>s")
+-- No Highlight
 keymap.set("n", "<leader>nh", ":nohl<CR>")
+-- [Plugin] neotree
+keymap.set('n', '<C-b>', ':Neotree toggle<CR>')
