7c076f8750ebdcbaccffe488ac27445afc58daa8
OrzMiku <miku@ecy.pink>
2024-07-11 22:39:29 +0800

    Fixed some errors in plugin configuration, added the alpha-nvim plugin.

--- lua/plugins/essential.lua
+++ lua/plugins/essential.lua
@@ -1,21 +1,13 @@
 return {
-  -- Theme
-  {
-    "olimorris/onedarkpro.nvim",
-    priority = 1000, -- Ensure it loads first
-  },
-  -- StatusBar
-  {
+ {
     'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    dependencies = {
+      'nvim-tree/nvim-web-devicons'
+    },
     opts = {
       theme = "onedark",  
-    },
-    config = function()
-      vim.cmd("colorscheme onedark")
-    end
+    }
   },
-  -- Winbar
 	{
   	"utilyre/barbecue.nvim",
   	name = "barbecue",
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,22 +1,34 @@
 return {
-  -- Bufferline
+  {
+    "olimorris/onedarkpro.nvim",
+    priority = 1000, -- Ensure it loads first
+    config = function()
+      vim.cmd("colorscheme onedark") 
+    end
+  }, 
   {
     'akinsho/bufferline.nvim',
     version = "*",
     dependencies = 'nvim-tree/nvim-web-devicons',
-    opts = {
-      theme = "onedark"
-    }
+    opts = {}
   },
-  -- Indent blankline
   {
     "lukas-reineke/indent-blankline.nvim",
     main = "ibl",
     opts = {}
   },
-  -- Gitsigins
   {
     "lewis6991/gitsigns.nvim",
     opts = {}
-  }
+  },
+  {
+    'goolord/alpha-nvim',
+    dependencies = {
+        'nvim-tree/nvim-web-devicons',
+        'nvim-lua/plenary.nvim'
+    },
+    config = function ()
+        require'alpha'.setup(require'alpha.themes.theta'.config)
+    end
+  };
 }
